1327d4ff874a799f1b7a240d7d75f69a6128a1d9
Renato Souza <renatovieiradesouza1@gmail.com>
2023-01-06 11:52:12 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,54 +1,32 @@
-#!/bin/bash
+sudo apt-get update
+sudo apt-get install -y curl git wget unzip libgconf-2-4 gdb libstdc++6 libglu1-mesa fonts-droid-fallback lib32stdc++6
+sudo python3 psmisc
+sudo apt-get clean
 
-export DEBIAN_FRONTEND=noninteractive
+sudo git clone https://github.com/flutter/flutter.git -b 3.3.2 /usr/local/flutter
 
-apt-get update && apt-get -yq install \
-  curl \ 
-  unzip \
-  libgconf-2-4 \
-  libnss3 \
-  libxss1 \
-  fonts-liberation \
-  libappindicator3-1 \
-  libasound2 \
-  libatk-bridge2.0-0 \
-  libgtk-3-0 \
-  libnspr4 \
-  libpango-1.0-0 \
-  libx11-xcb1 \
-  lsb-release \
-  wget \
-  tzdata \ 
-  git \
-  gdebi-core \
-  libnss3 \
-  libgconf-2-4 \
-  libgbm1 \
-  xdg-utils \
-  libgconf-2-4 \
-  gdb libstdc++6 \
-  libglu1-mesa \
-  fonts-droid-fallback \
-  lib32stdc++6
-  
-dpkg-reconfigure -f noninteractive tzdata
-apt-get clean
-git clone https://github.com/flutter/flutter.git -b stable
-export PATH=$PATH:/app/flutter/bin
-export PATH="${PATH}:/app/flutter/bin" 
+echo INSTALL CHROME
 
-wget https://chromedriver.storage.googleapis.com/108.0.5359.71/chromedriver_linux64.zip
-unzip chromedriver_linux64.zip
-rm chromedriver_linux64.zip
-mv chromedriver /usr/local/bin
-export PATH=$PATH
-  
-git config --global --add safe.directory /flutter
-cd flutter && git pull -f && flutter upgrade  
+sudo apt install wget
 
-wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-dpkg -i google-chrome-stable_current_amd64.deb
-apt-get install -f
-dpkg -i google-chrome-stable_current_amd64.deb
+sudo wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 
-flutter config --enable-web && flutter doctor
+sudo dpkg -i google-chrome-stable_current_amd64.deb
+
+sudo apt-get install -f
+
+google-chrome --version
+
+sudo wget https://chromedriver.storage.googleapis.com/108.0.5359.71/chromedriver_linux64.zip
+
+sudo unzip chromedriver_linux64.zip
+
+sudo mv chromedriver /usr/bin/chromedriver
+
+sudo chown root:root /usr/bin/chromedriver
+
+sudo chmod +x /usr/bin/chromedriver
+
+sudo chromedriver --port=4444 --detach=true &
+
+sudo /usr/local/flutter/bin/flutter drive --driver=/home/vsts/work/1/s/test_driver/main.dart --target=/home/vsts/work/1/s/integration_test/app_test.dart -d web-server
