129e3b08580f212d2808571caa9e7669d9f1ceac
Renato Souza <renatovieiradesouza1@gmail.com>
2023-01-06 11:48:46 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -47,6 +47,8 @@ git config --global --add safe.directory /flutter
 cd flutter && git pull -f && flutter upgrade  
 
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-gdebi -n google-chrome-stable_current_amd64.deb
+dpkg -i -n google-chrome-stable_current_amd64.deb
+apt-get install -f
+dpkg -i -n google-chrome-stable_current_amd64.deb
 
 flutter config --enable-web && flutter doctor
